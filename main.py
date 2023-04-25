653c5bbced8c95d00d810263d13f55d4bd9c743c
Gáll Péter <petigall00@gmail.com>
2023-04-25 11:40:23 +0200

    jó

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+# from _04_synthetic_data_polinomial import generate_synthetic data
+
+def fit_linear_regression(x, y):
+    lr = LinearRegression()
+    lr.fit(x.reshape(-1, 1), y)
 
 
 def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
@@ -8,11 +16,15 @@ def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
     return x, y
 
 
-def visualize_date(x, y):
+def visualize_data(x, y, model):
     plt.scatter(x, y)
     plt.xlabel("Feature (x)")
     plt.ylabel("Target (y)")
     plt.title("Synhetic Data with Polynomial Relationship and Noise")
+    x_pred = np.linspace(min(x), max(x), len(x)).reshape(-1, 1)
+    y_pred = model.predict(x_pred)
+    plt.plot(x_pred, y_pred, color='red', label='Linear Regression Fit')
+    plt.legend()
     plt.show()
 
 
@@ -20,4 +32,9 @@ def main():
     coefficients = [1, 0.02, -0.002, 0.014]
     x_values = np.linspace(-10, 10, 100)
     x, y = generate_synthetic_data(x_values, coefficients)
+    lr = fit_linear_regression(x, y)
     visualize_data(x, y)
+
+
+if __name__ == '__main__':
+    main()
