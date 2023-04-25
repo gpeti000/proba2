96574946a2f35a16458b5f0fba9a46c0beb05592
gpeti000 <67637657+gpeti000@users.noreply.github.com>
2023-04-25 11:03:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-
-print("Helloaaa")
-print("Hello World!")
+def visualize_date(x,y):
+    plt.scatter(x,y)
+    plt.xlabel("Feature (x)")
+    plt.ylabel("Target (y)")
+    plt.title("Synhetic Data with Polynomial Relationship and Noise")
+    plt.show()
+    
+def main():
+    coefficients = [1, 0.02, -0.002, 0.014]
+    x_values = np.linspace(-10, 10, 100)
+    x,y = generate_synthetic_data(x_values, coefficients)
+    visualize_data(x,y)
+    
